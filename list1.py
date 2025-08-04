9abcaba2fb04bf400ddc1313e63ef030e1e669a9
Vivekmore593. <viveksm593@gmail.com>
2025-08-04 13:59:40 +0530

    Added

---
+++ list1.py
@@ -0,0 +1,12 @@
+'''Sum of all Elements
+l1=[1,2,3,4,5]
+sum=0
+for i in l1:
+    sum=sum+i
+print("Sum of elements in list:",sum)'''
+'''ultiplication of all elements'''
+l1=[1,2,3,4,5]
+mul=1
+for i in l1:
+    mul=mul*i
+print("mul of elements in list:",mul)
